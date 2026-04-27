c67fd9cb6fd677a4f65c7b97e68c52ff8fac68ef
AnderCodex <skipperyt2020@gmail.com>
2026-04-27 19:12:51 -0300

    first commit

---
+++ src/test/java/uy/bios/proyecto/consultorio_odontologico/ConsultorioOdontologicoApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.bios.proyecto.consultorio_odontologico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioOdontologicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
